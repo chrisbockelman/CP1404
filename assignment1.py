c3c9a96653df36824584f369ea7d6bd1ca6e8e33
christopher.bockelman@my.jcu.edu.au <1epqs4qn>
2016-09-05 12:47:18 +1000

    Add completed add_item function

--- assignment1.py
+++ assignment1.py
@@ -36,6 +36,7 @@ def main():
         elif choice == "A":
             add_item()
         elif choice == "M":
+            display_list("r")
             change_type()
             print("Item marked as completed")
         else:
@@ -43,45 +44,82 @@ def main():
         choice = input(MENU).upper()
 
 #Displays list, for required, input = 'r', or for completed, input = 'c'
+#TODO Break list into individual parts for output and add price. STOP DISPLAYING WATCH PRICE/PRIORITY, SORT BY PRIORITY
 def display_list(input):
+    items = []
     file = open("items.csv", "r")
     count = 1
     for line_string in file:
-        if line_string[-1] == input:
-            item, price, priority, tag = line_string.split(",")
-            print("{}. {}\t ${:>5} ({})".format(count,item,price, priority))
+        item, price, priority, tag = line_string.strip().split(",")
+        items.append([item,float(price),priority,tag])
+
+    for item in items:
+        if item[3] == input:
+            print("{}. {}\t ${:>5.2} ({})".format(count, item, price, priority))
             count += 1
+
     file.close()
 
 #Adds new item to the list
 
-#FINISH THIS FUNCTION
 def add_item():
     file = open("items.csv", "a+")
-    item = input("Item name: ")
-    if item == "":
+    name = input("Item name: ")
+
+    #Add item name
+    if name == "":
         print("Input cannot be blank")
-        item = input("Item name: ")
+        name = input("Item name: ")
+
+
+    #Add item price
     try:
         price = float(input("Price: $"))
         if price < 0:
             print("Price must be >= $0")
+            price = float(input("Price: $"))
+
     except ValueError:
         print("Please enter a valid number")
         price = float(input("Price: $"))
+
+    #Add item priority
+    try:
+        priority = int(input("Priority (1 - High, 3 - Low): "))
+        if priority >= 1 and priority <= 3:
+            priority = priority
+        else:
+            priority = int(input("Please enter either 1, 2, or 3: "))
+
+    except ValueError:
+        priority = int(input("Please enter either 1, 2, or 3: "))
+
+
+    print("{},{},{},r".format(name.capitalize(), price, priority), file=file)
+
     file.close()
 
 
 #Changes required tag, 'r' to completed tag, 'c'
+#FINISH THIS FUNCTION
+#TODO Figure out choice option, infinite nested if?
 def change_type():
+    items = []
     file = open("items.csv", "a+")
     count = 1
+    countList = []
     for line_string in file:
-        item, price, priority, tag = line_string.split(",")
-        print("{}. {}\t ${:>5} ((})".format(count, item, price, priority))
+        item, price, priority, tag = line_string.strip().split(",")
+        items.append([item, float(price), priority, tag, count])
+        countList.append(count)
         count += 1
+
     try:
         choice = int(input("Enter the number of an item to mark as completed: "))
+        if choice in countList:
+            for item in items:
+                if items[-1] is choice:
+                    items[0][3] = "c"
 
     except ValueError:
         print("Not a valid number")
