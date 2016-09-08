24668f482be24d05d5c7ae909405caaf2510b2e5
christopher.bockelman@my.jcu.edu.au <1epqs4qn>
2016-09-08 23:05:56 +1000

    Sort by display_list by priority

--- assignment1.py
+++ assignment1.py
@@ -1,4 +1,5 @@
 #Shopping List 1.0 - by Chris Bockelman
+#Fair warning, items.csv randomly stops
 from operator import itemgetter
 MENU = "\nMenu:\n(R)equired items\n(C)ompleted items\n(A)dd item\n(M)ark item as completed\n(Q)uit"
 
@@ -24,7 +25,6 @@ def main():
         choice = input(MENU).upper()
 
 #Displays list, for required: input = 'r', or for completed: input = 'c'
-#TODO SORT BY PRIORITY.
 def display_list(menuChoice):
     items = []
     file = open("items.csv", "r")
@@ -33,11 +33,9 @@ def display_list(menuChoice):
         item, price, priority, tag = line_string.strip().split(",")
         items.append([item,float(price),int(priority),tag])
 
-
+    items.sort(key=itemgetter(0, 2))
 
     for item in items:
-        #TODO figure out itemgetter
-        items.sort(key=itemgetter(2))
         if item[3] == menuChoice:
             print("{}. {:20} ${:>6.2f} ({})".format(count, item[0], item[1], item[2]))
             count += 1
@@ -80,6 +78,7 @@ def add_item():
 
 
     print("{},{},{},r".format(name.capitalize(), price, priority), file=file)
+    print("{} has been added to the list.".format(name))
 
     file.close()
 
@@ -87,22 +86,29 @@ def add_item():
 #Changes required tag, 'r' to completed tag, 'c'
 #TODO Figure out choice option, infinite nested if?, return 'c' instead of 'r'
 def change_type():
-    items = []
     file = open("items.csv", "a+")
-    count = 1
+    dict = {}
     countList = []
-    for line_string in file:
-        item, price, priority, tag = line_string.strip().split(",")
-        items.append([item, float(price), priority, tag, count])
+    count = 1
+
+    for line_str in file:
+        item, price, priority, tag = line_str.strip().split(',')
+        dict[count] = [item, price, priority, tag]
         countList.append(count)
         count += 1
 
+    print(countList)
+    print(dict)
     try:
-        choice = int(input("Enter the number of an item to mark as completed: "))
-        if choice in countList:
-            for item in items:
-                if items[-1] is choice:
-                    items[0][3] = "c"
+        choice = int(input("Enter the number of an item to mark as completed:"))
+        while choice not in countList:
+            print("Invalid input")
+            choice = int(input("Enter the number of an item to mark as completed:"))
+
+        count = choice
+        dict[count][3] = "c"
+        print(tag.replace('r', 'c'), file=file)
+
 
     except ValueError:
         print("Not a valid number")
